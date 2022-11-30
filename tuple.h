655e5e3b33b79ceeb0dee8742c41bbec546ca03d
nicolastorres <nicolastorres@mailbox.org>
2022-12-01 00:49:25 +0100

    feat: implemented some of the operators

--- tuple.h
+++ tuple.h
@@ -2,39 +2,67 @@
 #define INC_07_RAY_TRACER_CHALLENGE_TUPLE_H
 
 #include <tuple>
+#include <cmath>
 
 class Tuple {
 private:
-    std::tuple<double, double, double, double> tuple;
+    double x,y,z,w;
 public:
-    bool isPoint() { return w() == 1.0; }
+    bool IsPoint()  const { return w == 1.0; }
 
-    bool isVector() { return w() == 0.0; }
+    bool IsVector() const { return w == 0.0; }
 
-    Tuple(double x, double y, double z, double w) {
-        tuple = std::make_tuple(x, y, z, w);
+    Tuple(double x, double y, double z, double w): x(x), y(y), z(z), w(w) {}
+
+    double X() const { return x; }
+
+    double Y() const { return y; }
+
+    double Z() const { return z; }
+
+    double W() const { return w; }
+
+    double Magnitude() const {
+        return std::sqrt(x*x + y*y + z*z + w*w);
     }
 
-    double x() { return std::get<0>(tuple); }
-
-    double y() { return std::get<1>(tuple); }
-
-    double z() { return std::get<2>(tuple); }
-
-    double w() { return std::get<3>(tuple); }
+    Tuple Normalized() const {
+        auto magnitude = Magnitude();
+        return {x/magnitude,y/magnitude,z/magnitude,w/magnitude};
+    }
 
     bool operator==(const Tuple &other) const {
-        return tuple == other.tuple;
+        return x == other.x && y == other.y && z == other.z && w == other.w;
     }
 
     bool operator!=(const Tuple &other) const {
-        return tuple != other.tuple;
+        return x != other.x || y != other.y || z != other.z || w != other.w;
+    }
+
+    Tuple operator+(const Tuple &other) const {
+        return {x + other.x, y + other.y, z + other.z, w + other.w};
+    }
+
+    Tuple operator-(const Tuple &other) const {
+        return {x - other.x, y - other.y, z - other.z, w - other.w};
+    }
+
+    Tuple operator-() const {
+        return {-x, -y, -z, -w};
+    }
+
+    Tuple operator*(const double& scalar) const {
+        return {x*scalar, y*scalar,z*scalar,w*scalar};
+    }
+
+    double operator*(const Tuple& other) const {
+        return x*other.x + y*other.y + z*other.z + w*other.w;
     }
 };
 
-Tuple point(double x, double y, double z) { return {x, y, z, 1.0}; }
+Tuple Point(double x, double y, double z) { return {x, y, z, 1.0}; }
 
-Tuple vector(double x, double y, double z) { return {x, y, z, 0.0}; }
+Tuple Vector(double x, double y, double z) { return {x, y, z, 0.0}; }
 
 
 #endif //INC_07_RAY_TRACER_CHALLENGE_TUPLE_H
--- tuple_test.cpp
+++ tuple_test.cpp
@@ -1,35 +1,118 @@
 #include <iostream>
 #include <gtest/gtest.h>
+#include <vector>
+#include <cmath>
 #include "tuple.h"
 
 TEST(chapter_one, tuple_with_w1_is_point) {
     auto t = Tuple(4.3, -4.2, 3.1, 1.0);
-    EXPECT_EQ(4.3, t.x());
-    EXPECT_EQ(-4.2, t.y());
-    EXPECT_EQ(3.1, t.z());
-    EXPECT_EQ(1.0, t.w());
-    EXPECT_TRUE(t.isPoint());
-    EXPECT_FALSE(t.isVector());
+    EXPECT_EQ(4.3, t.X());
+    EXPECT_EQ(-4.2, t.Y());
+    EXPECT_EQ(3.1, t.Z());
+    EXPECT_EQ(1.0, t.W());
+    EXPECT_TRUE(t.IsPoint());
+    EXPECT_FALSE(t.IsVector());
 }
 
 TEST(chapter_one, tuple_with_w0_is_vector) {
     auto t = Tuple(4.3, -4.2, 3.1, 0.0);
-    EXPECT_EQ(4.3, t.x());
-    EXPECT_EQ(-4.2, t.y());
-    EXPECT_EQ(3.1, t.z());
-    EXPECT_EQ(0.0, t.w());
-    EXPECT_FALSE(t.isPoint());
-    EXPECT_TRUE(t.isVector());
+    EXPECT_EQ(4.3, t.X());
+    EXPECT_EQ(-4.2, t.Y());
+    EXPECT_EQ(3.1, t.Z());
+    EXPECT_EQ(0.0, t.W());
+    EXPECT_FALSE(t.IsPoint());
+    EXPECT_TRUE(t.IsVector());
 }
 
 TEST(chapter_one, point_function_creates_point) {
     auto expected = Tuple(4, -4, 3, 1);
-    auto actual = point(4, -4, 3);
+    auto actual = Point(4, -4, 3);
     EXPECT_EQ(expected, actual);
 }
 
 TEST(chapter_one, vector_function_creates_vector) {
     auto expected = Tuple(4, -4, 3, 0);
-    auto actual = vector(4, -4, 3);
+    auto actual = Vector(4, -4, 3);
+    EXPECT_EQ(expected, actual);
+}
+
+TEST(chapter_one, adding_two_tuples) {
+    auto expected = Tuple(1,1,6,1);
+    auto actual = Tuple(3,-2,5,1) + Tuple(-2,3,1,0);
+    EXPECT_EQ(expected, actual);
+}
+
+TEST(chapter_one, subtracting_two_points) {
+    auto expected = Vector(-2,-4,-6);
+    auto actual = Point(3,2,1) - Point(5,6,7);
+    EXPECT_EQ(expected, actual);
+}
+
+TEST(chapter_one, subtracting_vector_from_point) {
+    auto expected = Point(-2,-4,-6);
+    auto actual = Point(3,2,1) - Vector(5,6,7);
+    EXPECT_EQ(expected, actual);
+}
+
+TEST(chapter_one, subtracting_two_vectors) {
+    auto expected = Vector(-2,-4,-6);
+    auto actual = Vector(3,2,1) - Vector(5,6,7);
+    EXPECT_EQ(expected, actual);
+}
+
+TEST(chapter_one, subtracting_vector_from_zero_vector) {
+    auto expected = Vector(-1,2,-3);
+    auto actual = Vector(0,0,0) - Vector(1,-2,3);
+    EXPECT_EQ(expected, actual);
+}
+
+TEST(chapter_one, negating_tuple) {
+    auto expected = Tuple(-1,2,-3,4);
+    auto actual = -Tuple(1,-2,3,-4);
+    EXPECT_EQ(expected, actual);
+}
+
+TEST(chapter_one, multiplying_tuple_by_scalar) {
+    auto expected = Tuple(3.5,-7,10.5,-14);
+    auto actual = Tuple(1,-2,3,-4) * 3.5;
+    EXPECT_EQ(expected, actual);
+}
+
+TEST(chapter_one, computing_magnitude_of_vector) {
+    std::vector<std::tuple<double, Tuple>> testData{
+        std::tuple<double, Tuple>(1, Vector(1,0,0)),
+        std::tuple<double, Tuple>(1, Vector(0,1,0)),
+        std::tuple<double, Tuple>(1, Vector(0,0,1)),
+        std::tuple<double, Tuple>(sqrt(14), Vector(1,2,3)),
+        std::tuple<double, Tuple>(sqrt(14), Vector(-1,-2,-3)),
+    };
+    for(auto tt: testData) {
+        auto expected = std::get<0>(tt);
+        auto actual = std::get<1>(tt).Magnitude();
+        EXPECT_EQ(expected, actual);
+    }
+}
+
+TEST(chapter_one, normalizing_vectors) {
+    std::vector<std::tuple<Tuple, Tuple>> testData{
+        std::tuple<Tuple, Tuple>(Vector(1,0,0), Vector(4,0,0)),
+        std::tuple<Tuple, Tuple>(Vector(1.0f/sqrt(14), 2.0f/sqrt(14), 3.0f/sqrt(14)), Vector(1,2,3)),
+    };
+    for(auto tt: testData) {
+        auto expected = std::get<0>(tt);
+        auto actual = std::get<1>(tt).Normalized();
+        EXPECT_EQ(expected, actual);
+    }
+}
+
+TEST(chapter_one, magnitude_of_normalized_vector) {
+    auto expected = 1;
+    auto actual = Vector(1,2,3).Normalized().Magnitude();
+    EXPECT_EQ(expected, actual);
+}
+
+TEST(chapter_one, dot_product_of_two_tuples) {
+    auto expected = 20;
+    auto actual = Vector(1,2,3) * Vector(2,3,4);
     EXPECT_EQ(expected, actual);
 }
